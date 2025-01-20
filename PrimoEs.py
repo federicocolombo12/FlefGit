2a797b97c3809c237933439f00e3150999f33e84
federicocolombo12 <chiccoclub5@gmail.com>
2025-01-20 23:19:48 +0100

    primo es

--- PrimoEs.py
+++ PrimoEs.py
@@ -1,3 +1,20 @@
-print("Hello World")
-print("Dioporco")
-print("Dioporco")
+infinitoFile = open(r"C:\Users\chicc\Documents\Creatività\StudioPythonFede\FlefGit\Infinito.txt", "r")
+infinitoString = infinitoFile.read()
+infinitoRighe=infinitoString.split("\n")
+parole=[]
+for riga in infinitoRighe:
+    parole+=riga.split(" ")
+
+for parola in parole:
+    parolaNew=parola.lower()
+    parolaNew=parolaNew.replace(",","")
+    parole[parole.index(parola)]=parolaNew
+
+dizionarioParole={}
+for parola in parole:
+    if parola in dizionarioParole:
+        dizionarioParole[parola]+=1
+    else:
+        dizionarioParole[parola]=1
+print(dizionarioParole)
+infinitoFile.close()
